f842506a14bd5c4d61fef93650437fefdecac3f0
iDrinkx <theophile.regner@gmail.com>
2026-02-18 15:56:04 +0100

    logo dans config

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ RUN npm install
 
 COPY . .
 
+VOLUME ["/config"]
+
 EXPOSE 3000
 
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(session({
 ========================= */
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("/config"));
 
 /* =========================
    GLOBAL USER (IMPORTANT)
