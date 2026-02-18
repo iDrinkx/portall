603eefbdd094930d5315b9bc60abb82a9d4035da
iDrinkx <theophile.regner@gmail.com>
2026-02-19 00:28:23 +0100

    feat: Improve data validation in stats processing and handle computing status

--- utils/cron-session-job.js
+++ utils/cron-session-job.js
@@ -24,17 +24,9 @@ function startSessionCronJob(TRACEARR_URL, TRACEARR_API_KEY, PLEX_URL, PLEX_TOKE
   });
 
   console.log("[CRON] Job session cache schedule: 0 2 * * * (tous les jours a 2h)");
-  
-  // Si le cache est vide, faire un pre-calcul au démarrage
-  const cacheKeys = SessionStatsCache.getKeys();
-  if (cacheKeys.length === 0 && TRACEARR_URL && TRACEARR_API_KEY) {
-    console.log("[CRON] Cache vide - pré-calcul des stats au démarrage...");
-    updateTracearrAllUsers(TRACEARR_URL, TRACEARR_API_KEY, PLEX_URL, PLEX_TOKEN)
-      .then(() => console.log("[CRON] Pré-calcul au démarrage termine"))
-      .catch(err => console.error("[CRON] Erreur pré-calcul:", err.message));
-  } else {
-    console.log("[CRON] Cache existant avec", cacheKeys.length, "utilisateurs");
-  }
+  console.log("[CRON] Cache existant avec", SessionStatsCache.getKeys().length, "utilisateurs");
+  console.log("[CRON] Les données seront mises à jour par le cron job chaque nuit à 2h");
+  console.log("[CRON] Les calculs à la demande (avec polling) seront faits lors de la première connexion d'un utilisateur");
   
   return cronJob;
 }
